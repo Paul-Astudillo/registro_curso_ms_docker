4c87687541821f4e7db8d0225ddeab8c61f5d890
Paul-Astudillo <129395539+Paul-Astudillo@users.noreply.github.com>
2023-12-19 20:54:40 -0500

    Initial commit

---
+++ src/main/java/ups/registro_curso_ms_docker/RegistroCursoMsDockerApplication.java
@@ -0,0 +1,13 @@
+package ups.registro_curso_ms_docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroCursoMsDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroCursoMsDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ups/registro_curso_ms_docker/RegistroCursoMsDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package ups.registro_curso_ms_docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroCursoMsDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
