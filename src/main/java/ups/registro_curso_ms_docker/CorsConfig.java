e22b5dce4b85116ba1fdebfeb8a3082036714b6a
Diego Tapia <jtapiav4@est.ups.edu.ec>
2023-12-21 11:34:05 -0500

    configuracion Dockerfile y docker-compose

--- src/main/java/ups/registro_curso_ms_docker/CorsConfig.java
+++ src/main/java/ups/registro_curso_ms_docker/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*") // "http://localhost:4200"
+                        .allowedOrigins("*") //http://localhost:4200
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowCredentials(true)
                         .maxAge(3600);
